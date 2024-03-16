ac9486ed56ede15fa86b298e85009e8dab19cde1
arin-s <sarkisianarin@gmail.com>
2024-03-17 03:54:31 +1100

    Completed exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(...args) {
+    //Check each args for valid type, size and value then sort the array.
+    if(args.length != 2) {
+        return "ERROR";
+    }
+    for(i in args) {
+        if(typeof args[i] !== "number")
+            return "ERROR";
+        if(args[i] < 0)
+            return "ERROR";
+    }
+    args = args.sort();
+    //
+    let result = 0;
+    for(let i = args[0]; i <= args[1]; i++) {
+        result += i;
+    }
+    return result;
 };
 
 // Do not edit below this line
