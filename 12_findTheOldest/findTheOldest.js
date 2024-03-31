1f4a4fec25ba3f5230e78a59a2c09639871d95d9
arin-s <sarkisianarin@gmail.com>
2024-04-01 05:25:15 +1100

    completed exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,23 @@
-const findTheOldest = function() {
+let currentYear = new Date().getFullYear();
 
+const findTheOldest = function(people) {
+  return people.reduce((oldest, person) => {
+    let age = calcAge(person);
+    if (age > calcAge(oldest))
+      return person;
+    return oldest;
+  }, {yearOfBirth: currentYear})
 };
+
+function calcAge(person)
+{
+  let yod = 0;
+    if(!("yearOfDeath" in person))
+      yod = currentYear;
+    else
+      yod = person.yearOfDeath;
+    return yod - person.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
