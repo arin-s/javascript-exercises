603c36070416981bfee501dced4d6c00335e124e
arin-s <sarkisianarin@gmail.com>
2024-04-01 01:09:21 +1100

    Completed exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	return numbers.reduce((total, number) => {return total += Number.parseInt(number);}, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+	return numbers.reduce((total, number) => {return total *= Number.parseInt(number);}, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let total = 1;
+	for (let i = 1; i <= number; i++) {
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
