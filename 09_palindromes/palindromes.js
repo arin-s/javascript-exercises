ed708c89d6f51359ba0fde4b82c5995cc3d08f23
arin-s <sarkisianarin@gmail.com>
2024-04-01 03:53:02 +1100

    Completed exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+  let str = input.replaceAll(/\W/g, "");
+  str = str.toLowerCase();
+  let len = Math.round((str.length - 1) / 2) - 1;
+  let valid = true;
+  for (let i = 0; i <= len; i++)
+    if(str[i] !== str[str.length - 1 - i]) 
+      valid = false;
+  return valid;
 };
 
 // Do not edit below this line
